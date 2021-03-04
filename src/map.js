ea525a632826370853541c11c54166ca7b432c86
Pranjal <bajajpranjal.1998@gmail.com>
2021-03-05 03:03:24 +0530

    Add. Logic to map a non-empty list based on given function

--- src/map.js
+++ src/map.js
@@ -1,5 +1,10 @@
 const map = (array, funcname) => {
     if(array.length==0)
         return array;
+    const map_res = [];
+    for (const val of array) {
+        map_res.push(funcname(val));
+    }
+    return map_res;
 }
 module.exports = map;
