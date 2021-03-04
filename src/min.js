91119fd3bc9e1ee4c22f1c2e19e51c92b7b62928
Pranjal <bajajpranjal.1998@gmail.com>
2021-03-05 03:48:31 +0530

    Add. Logic for a non-empty array minimum element

--- src/min.js
+++ src/min.js
@@ -2,6 +2,14 @@ const min = (array) =>
 {
         if(array.length==0)
             return undefined;
-         return null;
+        else if(array.length==1)
+            return array[0];
+        else if(array.length==2)
+            return (array[0]>array[1])?array[1]:array[0];
+        else
+        {
+            const m = min(array.slice(1));
+            return (array[0]<m)?array[0]:m;
+        }
 }
 module.exports=min;
--- src/min.spec.js
+++ src/min.spec.js
@@ -1,8 +1,12 @@
-const mins = require('./min');
+const min = require('./min');
 
 describe('Min',() => {
 
         it('Minimum element of empty array is undefined',() => {
-            expect(mins([])).toEqual(undefined);
+            expect(min([])).toEqual(undefined);
+        })
+
+        it('Minimum element of [1,2,3,4] is 1',() => {
+                expect(min([1,2,3,4])).toEqual(1);
         })
 })
