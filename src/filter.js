a8eb1e0ea098b9d7d1a06d5b5e43d76823265246
Pranjal <bajajpranjal.1998@gmail.com>
2021-03-05 03:15:28 +0530

    Add. Logic to filter a non-empty based on the function

--- src/filter.js
+++ src/filter.js
@@ -1,6 +1,11 @@
 const filter = (array,funcname) => {
     if(array.length==0)
         return array;
-    return null;
+    const filter_res = [];
+    for (const val of array) {
+        if(funcname(val))
+             filter_res.push(val);
+      }
+    return filter_res;
 }
 module.exports=filter;
