9f9c83e08f7805f1966b8d5368cdf59ff3334032
Pranjal <bajajpranjal.1998@gmail.com>
2021-03-05 03:00:44 +0530

    Add. test case for non-empty array map to identity function

--- src/map.spec.js
+++ src/map.spec.js
@@ -3,10 +3,16 @@ const map = require('./map');
 const cube = (a) => {
     return a*a*a;
 }
-
-
+const identity = (a) =>
+{
+    return a;
+}
 describe("Map", () => {
     it("Mapping of an empty array is []", () => {
         expect(map([],cube)).toEqual(expect.arrayContaining([]));
     })
+
+     it('Mapping of an array [1,2,3] with identity function is [1,2,3]',() => {
+            expect(map([1,2,3],identity)).toEqual(expect.arrayContaining([1,2,3]));
+        })
 })
