6833b9aaf90e7eda1276d9b522bba3f6119ae4c3
Pranjal <bajajpranjal.1998@gmail.com>
2021-03-05 03:05:23 +0530

    Add. Various test cases for a non-empty list

--- src/map.spec.js
+++ src/map.spec.js
@@ -14,5 +14,16 @@ describe("Map", () => {
 
      it('Mapping of an array [1,2,3] with identity function is [1,2,3]',() => {
             expect(map([1,2,3],identity)).toEqual(expect.arrayContaining([1,2,3]));
-        })
+     })
+
+     it('Mapping of an array [1,2,3] with cube function is [1,8,27]',() => {
+             expect(map([1,2,3],cube)).toEqual(expect.arrayContaining([1,8,27]));
+     })
+
+       it('Mapping of an array [a{x : 10}] with a->a.x+1 is [11]', () => {
+             const a = [{x:10}]
+             expect(map(a,function(a,x){
+                 return a.x=a.x+1;
+             })).toEqual(expect.arrayContaining([11]));
+         })
 })
